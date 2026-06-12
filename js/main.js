e9a73de03fba5cc0c89e2212277b7e6174af4b8d
sujitm355 <sujitm355@gmail.com>
2026-06-12 18:32:27 +0530

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,30 @@
+function selectOrderType(orderType) {
+    localStorage.setItem("orderType", orderType);
+    window.location.href = "menu.html";
+}
+
+function goToBookingPage() {
+    window.location.href = "booking.html";
+}
+
+function getUser() {
+    const user = localStorage.getItem("cafeUser");
+    return user ? JSON.parse(user) : null;
+}
+
+function setUser(user) {
+    localStorage.setItem("cafeUser", JSON.stringify(user));
+}
+
+function logout() {
+    localStorage.removeItem("cafeUser");
+    window.location.href = "login.html";
+}
+
+function requireLogin() {
+    if (!getUser()) {
+        window.location.href = "login.html";
+        return false;
+    }
+    return true;
+}
